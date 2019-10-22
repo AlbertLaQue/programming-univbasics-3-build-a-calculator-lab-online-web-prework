f528f1d110d3ce2e347e6b5cd903aea4f5fb7933
Albert LaQue <albertlaque@gmail.com>
2019-10-22 07:02:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,28 +3,23 @@ return num1 + num2
 end
 
 def subtraction(num1, num2)
-num1 = 5
-num2 = 10
 return num2 - num1 
 end
 
 def division(num1, num2)
-  num1 = 50
-  num2 = 2 
   return num1 / num2
 
 end
 
 def multiplication(num1, num2)
-num1 = 4
-num2 = 30
 return num1*num2
 end
 
 def modulo(num1, num2)
-
+return num1%num2
 end
 
 def square_root(num)
+  math.sqrt num
 
 end
